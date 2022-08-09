5fcac9c5a724d5028146a542360b4437ae4574e4
Igor Matsko <igormazsko@mail.ru>
2022-08-09 21:34:10 +0300

    add task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+using static System.Console;
+Clear();
+WriteLine("Введите первое число");
+int namberA = int.Parse(ReadLine());
+WriteLine("Введите второе число");
+int namberB = int.Parse(ReadLine());
+WriteLine("Введите третье число");
+int namberC = int.Parse(ReadLine());
+int m = namberA;
+if (namberB > m)
+{
+    m = namberB;
+}
+if (namberC > m)
+{
+    m = namberC;
+}
+Write("max = ");
+WriteLine(m);
