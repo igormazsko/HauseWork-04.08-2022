6a2812e67daca822f9cb5a98733bbdf76dd9b752
Igor Matsko <igormazsko@mail.ru>
2022-08-09 22:25:21 +0300

    add task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+using static System.Console;
+Clear();
+WriteLine("Введите число N");
+double namberN = Convert.ToDouble(ReadLine());
+double i = 1;
+while (i <= namberN)
+{
+    double namber = i % 2;
+    if (namber == 0)
+    {
+        WriteLine(i);
+    }
+    i++;
+}
