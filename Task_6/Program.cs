8eda3e388f4394678d31e2cb33dea6573d104628
Igor Matsko <igormazsko@mail.ru>
2022-08-09 22:00:58 +0300

    add task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+double namber = Convert.ToDouble(ReadLine());
+double namber1 = namber % 2;
+if (namber1 == 0)
+{
+    WriteLine("Да, чётное");
+    
+}
+else
+{
+    WriteLine("Нет, не четное");
+    
+}
